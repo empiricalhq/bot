00d9a5df8faa6be956f47189f3bda301a34a7980
Don David <dadch1404@gmail.com>
2025-09-24 09:23:10 -0500

    added GetEnvInt, GetEnvBool, GetEnvDuration to utils

--- pkg/utils/env.go
+++ pkg/utils/env.go
@@ -1,10 +1,47 @@
 package utils
 
-import "os"
+import (
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
 
-func GetEnv(key string, defaultValue string) string {
+func GetEnv(key, defaultValue string) string {
 	if value, exists := os.LookupEnv(key); exists {
 		return value
 	}
 	return defaultValue
+}
+
+func GetEnvInt(key string, defaultValue int) int {
+	str := GetEnv(key, "")
+	if str == "" {
+		return defaultValue
+	}
+	value, err := strconv.Atoi(str)
+	if err != nil {
+		return defaultValue
+	}
+	return value
+}
+
+func GetEnvBool(key string, defaultValue bool) bool {
+	str := GetEnv(key, "")
+	if str == "" {
+		return defaultValue
+	}
+	return strings.EqualFold(str, "true") || str == "1"
+}
+
+func GetEnvDuration(key string, defaultValue time.Duration) time.Duration {
+	str := GetEnv(key, "")
+	if str == "" {
+		return defaultValue
+	}
+	duration, err := time.ParseDuration(str)
+	if err != nil {
+		return defaultValue
+	}
+	return duration
 }
