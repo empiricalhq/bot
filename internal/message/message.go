f6fcd4349c91199b7450f2bf823df69f9ee5c9bc
Don David <dadch1404@gmail.com>
2025-09-24 09:21:28 -0500

    added message handler for whatsmeow

---
+++ internal/message/message.go
@@ -0,0 +1,50 @@
+package message
+
+import (
+	"fmt"
+
+	"go.mau.fi/whatsmeow/types"
+	"go.mau.fi/whatsmeow/types/events"
+)
+
+type Message struct {
+	Text      string
+	Sender    types.JID
+	Recipient types.JID
+	MessageID string
+}
+
+func New(evt *events.Message) *Message {
+	if evt.Info.IsGroup {
+		return nil // TODO: we should ignore group messages to avoid overloading the server
+	}
+
+	msg := &Message{
+		Sender:    evt.Info.Sender,
+		Recipient: evt.Info.Sender,
+		MessageID: evt.Info.ID,
+	}
+
+	// extract text content from various message types
+	if evt.Message.GetConversation() != "" {
+		msg.Text = evt.Message.GetConversation()
+	} else if extText := evt.Message.GetExtendedTextMessage(); extText != nil {
+		msg.Text = extText.GetText()
+	} else if img := evt.Message.GetImageMessage(); img != nil {
+		msg.Text = img.GetCaption()
+	}
+
+	return msg
+}
+
+func (m *Message) GetSenderID() string {
+	return m.Sender.ToNonAD().String()
+}
+
+func (m *Message) GetText() string {
+	return m.Text
+}
+
+func (m *Message) String() string {
+	return fmt.Sprintf("ID: %s, From: %s, Text: %q", m.MessageID, m.GetSenderID(), m.Text)
+}
