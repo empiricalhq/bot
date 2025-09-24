5cb6a7fee55b91c0851dc459a1e945cdf8a5f429
Don David <dadch1404@gmail.com>
2025-09-24 09:23:22 -0500

    include template renderer

---
+++ internal/templates/renderer.go
@@ -0,0 +1,20 @@
+package templates
+
+import "strings"
+
+type Renderer interface {
+	RenderText(template string, userName string) string
+}
+
+type TextRenderer struct{}
+
+func NewTextRenderer() *TextRenderer {
+	return &TextRenderer{}
+}
+
+func (r *TextRenderer) RenderText(template string, userName string) string {
+	if userName == "" {
+		userName = "Amig@"
+	}
+	return strings.ReplaceAll(template, "{{name}}", userName)
+}
